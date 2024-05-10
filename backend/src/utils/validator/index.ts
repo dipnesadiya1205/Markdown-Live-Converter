0487a5733921c3fb0dab5c228bee2fff05989155
dipnesadiya1205 <dipnesadiya0512@gmail.com>
2024-05-10 21:50:37 +0530

    backend architecture setup

---
+++ backend/src/utils/validator/index.ts
@@ -0,0 +1,17 @@
+// Custom Validators
+import {
+  isEmpty,
+  customRegex,
+  isDecimalNumber,
+  isNumber,
+  isString,
+} from "./customValidations";
+
+export {
+  // Custom Validations
+  isEmpty,
+  customRegex,
+  isDecimalNumber,
+  isNumber,
+  isString,
+};
